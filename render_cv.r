f9e552397aba901215c0e16bfef83fbbbe137f68
Emil Malta <emilmaltamuller@gmail.com>
2024-12-30 21:39:58 +0100

    Updated CV 2025

--- render_cv.r
+++ render_cv.r
@@ -6,16 +6,20 @@
 # for the HTML and PDF rendering. This exercise is left to the reader.
 
 # Knit the HTML version
-rmarkdown::render("cv.rmd",
+rmarkdown::render("cv.Rmd",
                   params = list(pdf_mode = FALSE),
                   output_file = "cv.html")
 
 # Knit the PDF version to temporary html location
 tmp_html_cv_loc <- fs::file_temp(ext = ".html")
-rmarkdown::render("cv.rmd",
+rmarkdown::render("cv.Rmd",
                   params = list(pdf_mode = TRUE),
-                  output_file = tmp_html_cv_loc)
+                  output_file = "temp_html.html")
 
 # Convert to PDF using Pagedown
-pagedown::chrome_print(input = tmp_html_cv_loc,
+pagedown::chrome_print(input = "temp_html.html",
                        output = "cv.pdf")
+
+
+
+
